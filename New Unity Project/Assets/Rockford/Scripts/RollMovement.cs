228b3d7c1e4295eb8051f0bda76db85501cfa348
rstoller0 <rockfordstoller@gmail.com>
2018-08-28 16:49:57 -0400

    Start of Camera Follow and Car Movement
    I have a good camera movement script and I started with a car movement script and is seems decent. More work is definately needed though. Everything I messed with is in my personal folder Assets/Rockford.

---
+++ New Unity Project/Assets/Rockford/Scripts/RollMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float motorForce, steerForce, brakeForce, slowForce;
+    [SerializeField]
+    private WheelCollider frWheel, flWheel, brWheel, blWheel;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //create/update acceleration and steering values
+        float acceleration = Input.GetAxis("Vertical") * motorForce;
+        float steerRotation = Input.GetAxis("Horizontal") * steerForce;
+
+        //apply acceleration
+        brWheel.motorTorque = acceleration;
+        blWheel.motorTorque = acceleration;
+
+        //apply steering
+        frWheel.steerAngle = steerRotation;
+        flWheel.steerAngle = steerRotation;
+
+        //user applied brakes
+        if (Input.GetKey(KeyCode.Space))
+        {
+            brWheel.brakeTorque = brakeForce;
+            blWheel.brakeTorque = brakeForce;
+        }
+
+        //car's natural slow without acceleration applied
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            brWheel.brakeTorque = slowForce;
+            blWheel.brakeTorque = slowForce;
+        }
+        else
+        {
+            brWheel.brakeTorque = 0;
+            blWheel.brakeTorque = 0;
+        }
+    }
+}
