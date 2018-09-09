3458a134bfae4e6c2f005079f6d377ef431db638
Michael Bonessi <mbonessi@uncc.edu>
2018-09-09 15:31:12 -0400

    Simple track generation

---
+++ New Unity Project/Assets/Michael/Scripts/TrackGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] tracks;
+    [SerializeField] private GameObject currentTrack;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Loop for testing
+        for(int i = 0; i < 10; i++)
+        {
+            SpawnTrack();
+        }
+	}
+    
+    //Picks a random piece of track to spawn at the end of the current track
+    void SpawnTrack()
+    {
+        int rand = Random.Range(0, tracks.Length);
+
+        currentTrack = Instantiate(tracks[rand], GetGrandchildPosition(currentTrack), Quaternion.identity);
+    }
+
+    //Gets the position of a child's child to clean the code up a little
+    Vector3 GetGrandchildPosition(GameObject obj)
+    {
+        return obj.transform.GetChild(0).transform.GetChild(0).position;
+    }
+}
