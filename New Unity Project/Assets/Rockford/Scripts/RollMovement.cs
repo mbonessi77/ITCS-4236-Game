f5c77e6c0b3c2dbeb2d906ad7931cfa4b5387f38
rstoller0 <rockfordstoller@gmail.com>
2018-09-10 18:54:36 -0400

    Camera and movement (Adjustments)
    I have just refined the movements of the car and camera. I also was looking at the main menu and track generation and noticed the link to load it was broken. But I believe I fixed that. Just removed the scenes and readded them to the build.

--- New Unity Project/Assets/Rockford/Scripts/RollMovement.cs
+++ New Unity Project/Assets/Rockford/Scripts/RollMovement.cs
@@ -8,25 +8,63 @@ public class RollMovement : MonoBehaviour
     private float motorForce, steerForce, brakeForce, slowForce;
     [SerializeField]
     private WheelCollider frWheel, flWheel, brWheel, blWheel;
+    [SerializeField]
+    private float topSpeed, currentSpeed;
+    [SerializeField]
+    private Rigidbody rb;
 
     void Start()
     {
-
+        
     }
 
     void Update()
     {
-        //create/update acceleration and steering values
-        float acceleration = Input.GetAxis("Vertical") * motorForce;
+        //create/update steering values
         float steerRotation = Input.GetAxis("Horizontal") * steerForce;
 
-        //apply acceleration
-        brWheel.motorTorque = acceleration;
-        blWheel.motorTorque = acceleration;
-
         //apply steering
-        frWheel.steerAngle = steerRotation;
-        flWheel.steerAngle = steerRotation;
+        if (currentSpeed > 10f)
+        {
+            frWheel.steerAngle = steerRotation * (10 / currentSpeed);
+            flWheel.steerAngle = steerRotation * (10 / currentSpeed);
+        }
+        else
+        {
+            frWheel.steerAngle = steerRotation;
+            flWheel.steerAngle = steerRotation;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        /*
+        float wheelRadius = brWheel.radius;
+        float wheelRPM = brWheel.rpm;
+        float circumference; //here we will store the circumference
+        float speedInMph; // and here the speed in mhiles in hour
+
+        circumference = 2.0f * 3.14f * wheelRadius; // Finding circumFerence 2 Pi R
+        speedInMph = ((circumference * wheelRPM) * 60) * 0.62f; // finding mph
+        */
+
+        //create/update acceleration values
+        float acceleration = Input.GetAxis("Vertical") * motorForce;
+
+        //store speed
+        currentSpeed = rb.velocity.magnitude;
+
+        //apply acceleration
+        if (currentSpeed < topSpeed)
+        {
+            brWheel.motorTorque = acceleration;
+            blWheel.motorTorque = acceleration;
+        }
+        else
+        {
+            brWheel.motorTorque = 0;
+            blWheel.motorTorque = 0;
+        }
 
         //user applied brakes
         if (Input.GetKey(KeyCode.Space))
@@ -46,5 +84,7 @@ public class RollMovement : MonoBehaviour
             brWheel.brakeTorque = 0;
             blWheel.brakeTorque = 0;
         }
+
+        //print(frWheel.steerAngle);
     }
 }
