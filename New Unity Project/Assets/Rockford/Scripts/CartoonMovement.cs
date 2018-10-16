931565ab92eb09694f672a8149b96d403df6bf2a
rstoller0 <rockfordstoller@gmail.com>
2018-10-16 10:39:10 -0400

    Working on movement more
    I am trying to get the movement to a better place. It needs a lot of work still. I will try to work on it tomorrow.

--- New Unity Project/Assets/Rockford/Scripts/CartoonMovement.cs
+++ New Unity Project/Assets/Rockford/Scripts/CartoonMovement.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CartoonMovement : MonoBehaviour
 {
+    /// <summary>
+    /// This needs a bit of work!!!!!!!!
+    /// </summary>
+
     [SerializeField]
     private float topSpeed, steer, brake;
     private float currentSpeed, acceleration;
+    private Vector3 localVelocity;
     [SerializeField]
     private Rigidbody rb;
     [SerializeField]
@@ -22,39 +27,24 @@ public class CartoonMovement : MonoBehaviour
         //store speed
         currentSpeed = rb.velocity.magnitude;
 
-        //create acceleration values
-        Vector3 towards = trans.forward;
-        Quaternion targetRotation = Quaternion.LookRotation(trans.forward);
-
-        //if steering right
-        if (Input.GetAxis("Horizontal") > 0.1f)
-        {
-            targetRotation = Quaternion.LookRotation(trans.forward + (trans.forward + trans.right));
-        }
-        else if(Input.GetAxis("Horizontal") < -0.1f)
-        {
-            //else if steering right
-            targetRotation = Quaternion.LookRotation(trans.forward + (trans.forward - trans.right));
-        }
-
         //determine correct acceleration value
         //if accelerating forwards
-        if (Input.GetAxis("Vertical") > 0.1f)
+        if (Input.GetAxis("Vertical") > 0.2f)
         {
-            //if going forwards and not at top speed, speed up backwards
+            //if going forwards and not at top speed, speed up forwards
             if (acceleration < topSpeed)
             {
-                acceleration += 0.1f;
+                acceleration += 0.2f;
             }
         }
-        else if (Input.GetAxis("Vertical") < -0.1f)
+        else if (Input.GetAxis("Vertical") < -0.2f)
         {
             //else if accelerating backwards
 
             //if going backwards and not at half of top speed in reverse, speed up backwards
             if (acceleration > -(topSpeed * 0.5f))
             {
-                acceleration -= 0.1f;
+                acceleration -= 0.2f;
             }
         }
         else
@@ -62,14 +52,14 @@ public class CartoonMovement : MonoBehaviour
             //else not accelerating
 
             //if not accelerating forwards or backwards and speed is above zero, slow down forward movement
-            if (acceleration > 0.05f)
+            if (acceleration > 0.1f)
             {
-                acceleration -= 0.05f;
+                acceleration -= 0.1f;
             }
-            else if (acceleration < -0.05f)
+            else if (acceleration < -0.1f)
             {
                 //else if not accelerating forwards or backwards and speed is below zero, slow down reverse movement
-                acceleration += 0.05f;
+                acceleration += 0.1f;
             }
             else
             {
@@ -77,6 +67,30 @@ public class CartoonMovement : MonoBehaviour
                 acceleration = 0;
             }
         }
+
+        //user applied brakes
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(acceleration > 0.4f)
+            {
+                acceleration -= 0.4f;
+            }
+            else if(acceleration < -0.4f)
+            {
+                acceleration += 0.4f;
+            }
+        }
+
+        //create variable to track speed and direction in the .z portion of the Vector3
+        localVelocity = rb.transform.InverseTransformDirection(rb.velocity);
+
+        //print(acceleration);
+    }
+
+    void FixedUpdate()
+    {
+        //create movement direction
+        Vector3 towards = trans.forward;
 
         //if not currently going max speed
         //update acceleration values/accelerate
@@ -90,22 +104,34 @@ public class CartoonMovement : MonoBehaviour
             rb.velocity = towards;
         }
 
-        //user applied brakes
-        if (Input.GetKey(KeyCode.Space))
+        //create acceleration values
+        Quaternion targetRotation = Quaternion.LookRotation(trans.forward);
+
+        //if steering right
+        if (Input.GetAxis("Horizontal") > 0.1f)
         {
-            if(acceleration > 0.2f)
-            {
-                acceleration -= 0.2f;
-            }
-            else if(acceleration < -0.2f)
-            {
-                acceleration += 0.2f;
-            }
+            targetRotation = Quaternion.LookRotation(trans.forward + (trans.forward + trans.right));
+        }
+        else if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            //else if steering right
+            targetRotation = Quaternion.LookRotation(trans.forward + (trans.forward - trans.right));
         }
 
         //rotate car to desired steering position
         trans.rotation = Quaternion.Lerp(trans.rotation, targetRotation, steer * Time.deltaTime);
+    }
 
-        print(rb.velocity.magnitude);
+    //BUGGY!!!!!!!!!!!!!!!!!!!
+    void OnCollisionStay(Collision other)
+    {
+        //if colliding with something other than the ground
+        if(other.transform.tag != "Ground")
+        {
+            if (Mathf.Abs(acceleration) > Mathf.Abs(localVelocity.z))
+            {
+                acceleration = localVelocity.z;
+            }
+        }
     }
 }
